0bf8acd664b462692a0ff1808d4f6a382bec1ada
Брянцев Александр <bryancev.a2@wildberries.work>
2023-12-23 01:42:29 +0300

    Переписал стили на styled components, добавил темый фон с переключателем

---
+++ src/global-styles.tsx
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+		body {
+      background-color: var(--theme);
+    }
+    html[data-theme=light] {
+				--theme: white;
+    }
+
+    html[data-theme=dark] {
+				--theme: black;
+    }
+
+`;
+
+export default GlobalStyles;
--- src/pages/confidential/index.tsx
+++ src/pages/confidential/index.tsx
@@ -1,9 +1,21 @@
-const Confidential = () => {
-    return (
-        <>
-            <div>Secret</div>
-        </>
-    )
+import styled from "styled-components";
+
+export interface StyledProps {
+  Height?: string;
 }
+
+const ConfidentialWrapper = styled.div<StyledProps>`
+  color: white;
+  font-size: ${({ Height }) => (Height ? Height : "10px")};
+  text-align: center;
+`;
+
+const Confidential = () => {
+  return (
+    <>
+      <ConfidentialWrapper Height="100px">Secret Route</ConfidentialWrapper>
+    </>
+  );
+};
 
 export default Confidential;
