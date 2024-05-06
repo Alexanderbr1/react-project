76fc1d8d754d95d90db5f7b1709df74e07ca574d
Брянцев Александр <bryancev.a2@wildberries.work>
2024-05-06 14:17:54 +0300

    feat: add template pdf component + pdf-generator

---
+++ src/components/PdfTemplate/PdfTemplate.tsx
@@ -0,0 +1,38 @@
+import {FC} from "react";
+import {Document, Image, Page, StyleSheet, Text, View} from "@react-pdf/renderer";
+
+interface IPdfTemplateProps {
+    name: string;
+    picture: string;
+}
+
+const styles = StyleSheet.create({
+    page: {
+        flexDirection: 'row',
+        backgroundColor: '#E4E4E4'
+    },
+    section: {
+        margin: 10,
+        padding: 10,
+        flexGrow: 1
+    }
+});
+
+const  PdfTemplate: FC<IPdfTemplateProps> = ({ name, picture }) => {
+    console.log(picture)
+    return (
+        <Document>
+            <Page size="A4" style={styles.page}>
+                <View style={styles.section}>
+                    <Text>{name}</Text>
+                </View>
+
+                <View style={styles.section}>
+                    {picture && <Image src={picture[0]} />}
+                </View>
+            </Page>
+        </Document>
+    )
+}
+
+export default PdfTemplate;
---
+++ src/pages/pdf/index.tsx
@@ -0,0 +1,99 @@
+import {useState} from "react";
+import {PDFDownloadLink} from "@react-pdf/renderer";
+import {Controller, useForm} from "react-hook-form";
+import PdfTemplate from "../../components/PdfTemplate/PdfTemplate.tsx";
+import {Alert, Button, Input} from "antd";
+import styled from "styled-components";
+
+interface IPdfForm {
+    name: string;
+    picture: string;
+}
+
+const FormWrapper = styled.div`
+  height: 60vh;
+  display: flex;
+    flex-direction: column;
+  justify-content: center;
+  align-items: center;
+    gap: 10px;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+  }
+`;
+
+const PdfGenerator = () => {
+    const [task, setTask] = useState<IPdfForm>()
+
+    const {
+        control,
+        register,
+        handleSubmit,
+        formState: {errors, isValid},
+    } = useForm<IPdfForm>({
+        mode: "onBlur",
+    })
+
+    const saveElement = (data: IPdfForm) => {
+        setTask(data)
+    }
+
+    return(
+        <>
+            <FormWrapper>
+                <form onSubmit={handleSubmit(saveElement)}>
+                    <Controller
+                        control={control}
+                        rules={{
+                            required: true,
+                        }}
+                        render={({ field: { onChange, onBlur, value } }) => (
+                            <Input
+                                placeholder="Name"
+                                onBlur={onBlur}
+                                onChange={onChange}
+                                value={value}
+                            />
+                        )}
+                        name="name"
+                    />
+                    {errors?.name &&
+                        <Alert message="Поле обязательно для заполнения" type="error" showIcon/>
+                    }
+
+                    <input
+                        type="file"
+                        accept="image/*"
+                        {...register("picture", {
+                            required: "Поле обязательно для заполнения"
+                        })}
+                    />
+
+                    {errors?.picture &&
+                        <Alert message={ errors.picture?.message} type="error" showIcon/>
+                    }
+                    <Button type="primary" htmlType="submit" disabled={!isValid}>Сохранить</Button>
+                </form>
+                {
+                    !!task?.name &&
+                    <PDFDownloadLink
+                        document={
+                            <PdfTemplate
+                                name={task.name}
+                                picture={task.picture}
+                            />
+                        }
+                        fileName="file.pdf"
+                    >
+                        {({blob, url, loading, error}) => (loading ? 'Загрузка...' : 'Скачать')}
+                    </PDFDownloadLink>
+                }
+            </FormWrapper>
+        </>
+    )
+}
+
+export default PdfGenerator
